657bd2023338efc7ff4a98bc8ff5b92113e91f77
mohammedshetaya <mohammedshetaya22@gmail.com>
2022-12-08 17:43:43 +0200

    Changes by Samer

--- src/code/CoastGuard.java
+++ src/code/CoastGuard.java
@@ -48,7 +48,9 @@ public class CoastGuard {
        // String grid="7,5;40;2,3;3,6;1,1,10,4,5,90;";
         // String grid=GridGenerator.genGrid();
         // String grid="9,13;65;6,4;3,8,6,2,2,2;1,12,88,7,4,45,2,9,41,5,8,12,7,12,96;";
-        String grid0 = "5,6;50;0,1;0,4,3,3;1,1,90;";
+
+
+        String grid0 = "5,6;50;0,1;0,4,3,3;1,1,20;";
         String grid1 = "6,6;52;2,0;2,4,4,0,5,4;2,1,19,4,2,6,5,0,8;";
         String grid2 = "7,5;40;2,3;3,6;1,1,10,4,5,90;";
         String grid3 = "8,5;60;4,6;2,7;3,4,37,3,5,93,4,0,40;";
@@ -59,6 +61,19 @@ public class CoastGuard {
         String grid8 = "6,6;74;1,1;0,3,1,0,2,0,2,4,4,0,4,2,5,0;0,0,78,3,3,5,4,3,40;";
         String grid9 = "7,5;100;3,4;2,6,3,5;0,0,4,0,1,8,1,4,77,1,5,1,3,2,94,4,3,46;";
         String grid10= "10,6;59;1,7;0,0,2,2,3,0,5,3;1,3,69,3,4,80,4,7,94,4,9,14,5,2,39;";
-        System.out.println(solve(grid10,"GR2",true));
+
+        //System.out.println(solve(grid0,"AS1",true));
+        System.out.println(solve(grid1,"AS1",true));
+        //System.out.println(solve(grid2,"AS1",true));
+        //System.out.println(solve(grid3,"AS2",true));
+        //System.out.println(solve(grid4,"AS2",true));
+        //System.out.println(solve(grid5,"AS1",true));
+        //System.out.println(solve(grid6,"AS1",true));
+        //System.out.println(solve(grid7,"AS1",true));
+        //System.out.println(solve(grid8,"AS1",true));
+        //System.out.println(solve(grid9,"AS1",true));
+        //System.out.println(solve(grid10,"AS1",true));
+
+
     }
 }
